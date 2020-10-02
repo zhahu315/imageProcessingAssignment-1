6812c87d7a88f350f97217dd6132a3f13ed91201
zhangchi <1120286713@qq.com>
2020-10-02 16:03:59 +0800

    improve the code

--- 4/main.py
+++ 4/main.py
@@ -1,16 +1,28 @@
 import math
 import numpy as np
 import sys
-from conv import Conv
 
 
 def gaussKernel(sig, m=-1):
 	min_m = math.ceil(3 * sig) * 2 + 1
-	if m == -1:
-		m = min_m
-		print(m)
-	elif 0 < m < min_m:
+	if 0 < m < min_m:
 		print('the m is too small')
+	elif m == -1:
+		m = min_m
+		print('m=', m)
+		w = np.zeros([m, m], dtype=float)
+		sum = 0.
+		for i in range(m):
+			for j in range(m):
+				w[i][j] = (1 / (2 * math.pi * sig)) \
+				          * (math.exp(-pow((j - int(m / 2)), 2)\
+				                      - pow(i - int(m / 2), 2)\
+				                      / (2 * pow(sig, 2))))
+				sum += w[i][j]
+		for i in range(m):
+			for j in range(m):
+				w[i][j] /= sum
+		print(w)
 	else:
 		w = np.zeros([m, m], dtype=float)
 		sum = 0.
@@ -25,7 +37,7 @@ def gaussKernel(sig, m=-1):
 			for j in range(m):
 				w[i][j] /= sum
 		print(w)
-	Conv.twodConv('1.2.06.tiff', w, 'replicate')
+	return w
 
 
 if __name__ == '__main__':
--- 5/gausskernel.py
+++ 5/gausskernel.py
@@ -5,11 +5,24 @@ import sys
 
 def gaussKernel(sig, m=-1):
 	min_m = math.ceil(3 * sig) * 2 + 1
-	if m == -1:
-		m = min_m
-		print(m)
-	elif 0 < m < min_m:
+	if 0 < m < min_m:
 		print('the m is too small')
+	elif m == -1:
+		m = min_m
+		print('m=', m)
+		w = np.zeros([m, m], dtype=float)
+		sum = 0.
+		for i in range(m):
+			for j in range(m):
+				w[i][j] = (1 / (2 * math.pi * sig)) \
+				          * (math.exp(-pow((j - int(m / 2)), 2) \
+				                      - pow(i - int(m / 2), 2) \
+				                      / (2 * pow(sig, 2))))
+				sum += w[i][j]
+		for i in range(m):
+			for j in range(m):
+				w[i][j] /= sum
+		print(w)
 	else:
 		w = np.zeros([m, m], dtype=float)
 		sum = 0.
@@ -28,5 +41,5 @@ def gaussKernel(sig, m=-1):
 
 
 if __name__ == '__main__':
-	gaussKernel(5, 31)
+	gaussKernel(1, 7)
 	sys.exit(0)
